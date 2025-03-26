fdbfaffba36ae789f81f8d7d5fcccc5c7e7ff456
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-03-26 11:20:40 -0400

    confirm the resolved config

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,6 +2,21 @@
 
 beforeEach(() => {
   cy.log('in the before each')
+
+  // the hook config has resolved values
+  // from the declaration in the describe block and the test itself
+  expect(Cypress.config('foo')).to.equal('bar')
+  expect(Cypress.config('baz')).to.equal('quux')
 })
 
-it('has the config', () => {})
+describe(
+  'Parent suite',
+  { baz: 'quux', foo: 'some other value' },
+  () => {
+    // overwrite the "foo" config for this test
+    it('has the config', { foo: 'bar' }, () => {
+      expect(Cypress.config('foo')).to.equal('bar')
+      expect(Cypress.config('baz')).to.equal('quux')
+    })
+  },
+)
